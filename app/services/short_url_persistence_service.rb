22c536b8c207cebfbe627dd266e1a7e86102f7db
Dylan Runkel <dylan.runkel@gmail.com>
2018-04-13 09:30:01 -0700

    Change persistence service to simply take url, not short_url object

--- app/services/short_url_persistence_service.rb
+++ app/services/short_url_persistence_service.rb
@@ -1,30 +1,32 @@
-# Service that handles saving ShortUrls.
+# Service that handles creating new ShortUrls.
 # Handles the case of collisions in the actual
 # shortened urls.
+# Returns the new short_url
 class ShortUrlPersistenceService
   # How many times we should retry during collisions
   MAX_ATTEMPS = 5
 
   attr_reader :errors
 
-  def initialize(short_url)
-    @short_url = short_url
+  def initialize(url)
+    @url = url
     @errors = []
   end
 
   # Attempt to save the ShortUrl and handle case of collisions
   def save!
     attemps = 0
+    short_url = ShortUrl.new(original_url: @url)
     begin
-      @short_url.save!
+      short_url.save!
+      short_url
     rescue ActiveRecord::RecordNotUnique => e
       raise e if attemps >= MAX_ATTEMPS
 
-      # If we have retries left, clear the short_url
-      # on the object and re-attempt save.
+      # If we have retries left, re-initialize model.
       # Model will re-generate a short_url
       attemps += 1
-      @short_url.short_url = nil
+      short_url = ShortUrl.new(original_url: @url)
       retry
     rescue StandardError => e
       @errors << e.message
